9fe1d84788fcb322e1928159ed6f7f97448f6f74
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-05-31 12:48:35 +0800

    add header and separate file.

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <q-header flat>
+      <q-toolbar class="text-white bg-indigo-8">
+        <q-btn flat round dense icon="mic" />
+        <q-toolbar-title>Voice Caster</q-toolbar-title>
+        <q-space />
+        <div>VoiceCaster <q-badge color="yellow">v1.0.0</q-badge></div>
+        <q-btn flat round dense icon="speaker" />
+      </q-toolbar>
+    </q-header>
+</template>
+
