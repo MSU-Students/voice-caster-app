870c2698e61bf5bae06d1ca88b163a1207817da8
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-06-25 15:44:16 +0800

    updated.

--- src/services/audio-recorder.service.js
+++ src/services/audio-recorder.service.js
@@ -1,30 +1,28 @@
 class AudioRecorderService {
-    async recordAudio() {
-      await navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-        const mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.start();
-        const audioChunks = [];
+  async recordAudio() {
+    await navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      const audioChunks = [];
 
-        mediaRecorder.addEventListener("dataavailable", event => {
-          audioChunks.push(event.data);
-          console.log("recording: ", audioChunks);
-        });
-
-        mediaRecorder.addEventListener("stop", () => {
-          const audioBlob = new Blob(audioChunks);
-          const audioURL = URL.createObjectURL(audioBlob);
-          const audio = new Audio(audioURL);
-          audio.play();
-        });
-
-        setTimeout(() => {
-          mediaRecorder.stop();
-        }, 3000);
+      mediaRecorder.addEventListener("dataavailable", event => {
+        audioChunks.push(event.data);
+        console.log("recording: ", audioChunks);
       });
-    }
 
+      mediaRecorder.addEventListener("stop", () => {
+        const audioBlob = new Blob(audioChunks);
+        const audioURL = URL.createObjectURL(audioBlob);
+        const audio = new Audio(audioURL);
+        audio.play();
+      });
+
+      setTimeout(() => {
+        mediaRecorder.stop();
+      }, 3000);
+    });
+  }
 }
 
 let audioRecorderService = new AudioRecorderService();
-
 export default audioRecorderService;
