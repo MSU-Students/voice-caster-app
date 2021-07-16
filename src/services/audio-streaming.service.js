419d7d51c63a4c00164b0899ee4fd4286b95264c
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-07-17 04:40:49 +0800

    fixed size limit content & hide logs.

--- src/services/audio-streaming.service.js
+++ src/services/audio-streaming.service.js
@@ -3,32 +3,18 @@ class AudioStreamingService {
     console.log("starting...");
     await navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
       this.mediaRecorder = new MediaRecorder(stream);
-      const duration = 1000;
+      const duration = 5000;
       this.mediaRecorder.start(duration);
       const audioChunks = [];
 
       this.mediaRecorder.addEventListener("dataavailable", event => {
-        audioChunks.push(event.data);
-        const audioBlob = new Blob(audioChunks);
+        const audioBlob = new Blob([event.data]);
         var reader = new FileReader();
         reader.readAsDataURL(audioBlob);
         reader.onloadend = function() {
-          console.log("here: ", reader);
-          // callback(JSON.stringify({
-          //   data: reader.result,
-          //   time: (new Date()).getDate(),
-          //   duration: duration
-          // }));
           callback(reader.result);
         };
       });
-
-      // this.mediaRecorder.addEventListener("stop", () => {
-      // const audioURL = URL.createObjectURL(audioBlob);
-      // const audio = new Audio(audioURL);
-      // audio.play();
-      //audioBlob.text().then(callback)
-      // });
     });
   }
   async stop() {
