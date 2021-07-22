6137be9cb5ae7465082e1ffa6712ac2c9c3af168
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-07-23 02:54:05 +0800

    add button for manage network

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,28 @@
 <template>
-    <q-header style="background: linear-gradient(145deg, #15503e 15%, #133154 70%);">
-      <q-toolbar>
-        <q-avatar size="50px">
-            <img src="~assets/VoiceCasterBEX.png">
-          </q-avatar>
-        <q-toolbar-title>Voice Caster</q-toolbar-title>
-       
-        <q-badge color="white" text-color="black">v0.0.1</q-badge>
-        <q-btn flat round dense icon="speaker" />
-      </q-toolbar>
-    </q-header>
+  <q-header
+    style="background: linear-gradient(145deg, #15503e 15%, #133154 70%);"
+  >
+    <q-toolbar>
+      <q-avatar size="50px">
+        <img src="~assets/VoiceCasterBEX.png" />
+      </q-avatar>
+      <q-toolbar-title>Voice Caster</q-toolbar-title>
+      <q-btn
+        class="text-overline"
+        outline
+        rounded
+        icon="settings"
+        color="white"
+        label="Manage Network"
+        @click="() => $router.replace('/office')"
+        no-caps
+      >
+      </q-btn>
+    </q-toolbar>
+  </q-header>
 </template>
-
+<script>
+export default {
+  
+}
+</script>
