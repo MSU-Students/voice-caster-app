39d72ad68adb67af4e5be815ea19e7eae8a83748
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-07-23 03:20:27 +0800

    add itemCheck func. and addServerIP

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,15 +14,81 @@
         icon="settings"
         color="white"
         label="Manage Network"
-        @click="() => $router.replace('/office')"
+        @click="dialog = true"
         no-caps
       >
       </q-btn>
     </q-toolbar>
+    <q-dialog
+      v-model="dialog"
+      persistent
+      transition-show="scale"
+      transition-hide="scale"
+    >
+      <q-card class="text-dark" style="width: 300px">
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-weight-bold">Server Setting</div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+        <q-card-section>
+          <q-form @submit="save()" class="q-gutter-xs">
+            <q-input
+              dense
+              filled
+              v-model="server.ipAddress"
+              label="IP Address"
+              hint="ex. 192.168.100.1"
+              lazy-rules
+              :rules="[
+                val => (val && val.length > 0) || 'Please type something.'
+              ]"
+            />
+
+            <q-input
+              dense
+              filled
+              v-model="server.port"
+              label="Port"
+              hint="ex. 8080"
+              lazy-rules
+              :rules="[
+                val => (val !== null && val !== '') || 'Please type port #.'
+              ]"
+            />
+
+            <div>
+              <q-btn
+                :loading="showSaveLoader"
+                class="full-width"
+                label="Save"
+                type="submit"
+                color="indigo"
+              >
+              </q-btn>
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-header>
 </template>
 <script>
+import serverConnectionService from "../services/server-connection.service.js"
 export default {
-  
-}
+  data() {
+    return {
+      dialog: false,
+      showSaveLoader: false,
+      server: {
+        ipAddress: null,
+        port: null
+      },
+      server_ip: {}
+    };
+  },
+  created() {
+    serverConnectionService.isItemExist("server_ip");
+  },
+};
 </script>
--- src/services/server-connection.service.js
+++ src/services/server-connection.service.js
@@ -52,6 +52,19 @@ class ServerConnectionService {
   async tickleConnection() {
     (await this.isConnected) ? this.disconnect() : this.connect();
   }
+  async isItemExist(itemName) {
+    if (localStorage.getItem(itemName) !== null) {
+      console.log(`IP exist`);
+    } else {
+      console.log(`ip not found`);
+      let ip = JSON.stringify("");
+      this.addServerIP("server_ip", ip);
+    }
+  }
+  async addServerIP(key, val) {
+    let ip = JSON.stringify(val);
+    return await localStorage.setItem(key, ip);
+  }
 }
 
 let serverConnectionService = new ServerConnectionService();
